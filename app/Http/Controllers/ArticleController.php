f7ac1184c9d0ecd58f499cba1bdeb1c47c29281e
mae616 <oya2.mi@hotmail.co.jp>
2022-01-09 19:28:17 +0900

    記事更新: viewの作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -28,4 +28,8 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);    
+    }
 }
