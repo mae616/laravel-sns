961822fc0f5a03e7b1154ba9099f71a1fc7079fc
mae616 <oya2.mi@hotmail.co.jp>
2022-01-16 12:52:01 +0900

    タグ: 記事投稿でのタグ登録(vueへの組み込み)

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -27,7 +27,7 @@ class ArticleRequest extends FormRequest
             'title' => 'required|max:50',
             'body' => 'required|max:500',
             // tagsがJson形式でスペース　/^(?!.*\s).+$\u　と/　/^(?!.*\/).*$/uを含まないか
-            'tags' => 'json|regex:/^(?!.*\s).+$\u|regex:/^(?!.*\/).*$/u'
+            'tags' => 'json|regex:/^(?!.*\s).+$/u|regex:/^(?!.*\/).*$/u'
         ];
     }
 
