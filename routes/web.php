9088bc42cd247f58beb465b454bd0d7ff95b2dfb
mae616 <oya2.mi@hotmail.co.jp>
2022-01-18 13:11:14 +0900

    googleでログイン: ログインのコールバックの実装

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Auth::routes();
 
 Route::prefix('login')->name('login.')->group(function () {
   Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+  Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
 
 // ArticleControllerコントローラでindexメソッドを実行する
